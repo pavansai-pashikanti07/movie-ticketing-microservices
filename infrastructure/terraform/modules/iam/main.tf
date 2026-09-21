61ba9024492bfe875658f497e7285f286c33b95d
pavansai pashikanti <pavansai.pashikanti@gmail.com>
2026-09-22 00:35:35 +0530

    fix(terraform): use official GitHub Actions OIDC thumbprints and case-tolerant sub claim

--- infrastructure/terraform/modules/iam/main.tf
+++ infrastructure/terraform/modules/iam/main.tf
@@ -280,7 +280,10 @@ data "tls_certificate" "github" {
 resource "aws_iam_openid_connect_provider" "github_actions" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f8d731d304"
+  ]
 
   tags = merge(var.tags, { Name = "${var.name}-github-oidc" })
 }
@@ -299,7 +302,10 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:${var.github_repo}:*"]
+      values   = [
+        "repo:${var.github_repo}:*",
+        "repo:${lower(var.github_repo)}:*"
+      ]
     }
 
     principals {
