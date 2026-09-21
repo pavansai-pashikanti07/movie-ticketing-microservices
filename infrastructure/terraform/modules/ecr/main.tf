5f3388d8865c7cf875f93c9baeb933a2b783dc98
pavansai pashikanti <pavansai.pashikanti@gmail.com>
2026-09-22 01:29:22 +0530

    fix(ecr): add force_delete=true to prevent destroy failure on non-empty repos

--- infrastructure/terraform/modules/ecr/main.tf
+++ infrastructure/terraform/modules/ecr/main.tf
@@ -3,6 +3,7 @@ resource "aws_ecr_repository" "ecr_repository" {
 
   name                 = each.key
   image_tag_mutability = var.image_tag_mutability
+  force_delete         = true  # allows destroy even when images exist
 
   image_scanning_configuration {
     scan_on_push = var.scan_on_push
